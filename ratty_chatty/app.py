8a1eab45ea14b30a6655c17b64a8dcccb2be07c1
Stefan Eng <eng_steff@outlook.com>
2021-05-12 18:11:25 +0200

    Add self-hosted socket.io lib and use for index.html conn-msg

--- ratty_chatty/app.py
+++ ratty_chatty/app.py
@@ -1,14 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-noes-secrets!"
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-  return "HELLO"
+  return render_template('index.html')
 
+@socketio.on('message')
+def handle_message(data):
+  print(f"received message: {data}")
 
 def run():
   os.environ['FLASK_ENV'] = "development"
-  app.run("0.0.0.0", debug=True)
+  socketio.run(app, "0.0.0.0", debug=True)
